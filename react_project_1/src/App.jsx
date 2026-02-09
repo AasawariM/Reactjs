c3ddfcebed5ea6a424ebff0fe242007520ab3f03
AasawariM <aasawari.m.work@gmail.com>
2026-02-09 15:15:57 +0530

    props and jsx

--- react_project_1/src/App.jsx
+++ react_project_1/src/App.jsx
@@ -1,12 +1,30 @@
 import "./App.css";
 import UserCard from "./components/UserCard";
+import belly from "./assets/belly_girl.png";
+import jeremiah from "./assets/jeremiah_boy.png";
+import conrad from "./assets/conrad.avif";
 
 function App() {
   return (
     <div className="container">
-      <UserCard />
-      <UserCard />
-      <UserCard />
+      {/* passing/transfering data with components call
+      normal html mei is prakar tag ke andher likhe hue ko attribute kehte and in jsx it is called prop.
+      // we are passing name desc img and style as props below
+      */}
+      <UserCard
+        name="Conrad Fisher"
+        img={conrad}
+        description="I'm a Doctor"
+        style={{ borderRadius: "30px", backgroundColor: "#b1ea92" }}
+      />
+      <UserCard
+        name="Belly"
+        img={belly}
+        description="I'm a student"
+        style={{ backgroundColor: "#f8f064" }}
+      />
+      {/* this will take border radius and bg color from userCard.css */}
+      <UserCard name="Jeremiah" img={jeremiah} description="I'm a Cook" />
     </div>
   );
 }
--- react_project_1/src/components/UserCard.jsx
+++ react_project_1/src/components/UserCard.jsx
@@ -1,14 +1,15 @@
 import "./UserCard.css";
-const UserCard = () => {
+// we want different data for each UserCard call so we need to make it dynamic.
+// this is done using props = used for data passing between components.
+// receieving prop = UserCard ke andher data aa raha hoga props naam ka
+// to access props we use curly braces {}.
+// just like text and image we can also pass styles as props
+const UserCard = (props) => {
   return (
-    <div className="user-container">
-      <h1 id="user-name">John Doe</h1>
-      <img
-        src="https://plus.unsplash.com/premium_vector-1727955579176-073f1c85dcda?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        id="user-image"
-        alt="profile photo"
-      />
-      <p id="user-desc">Description of John Doe</p>
+    <div className="user-container" style={props.style}>
+      <h1 className="user-name">{props.name}</h1>
+      <img src={props.img} className="user-image" alt="profile photo" />
+      <p className="user-desc">{props.description}</p>
     </div>
   );
 };
